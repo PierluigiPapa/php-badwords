1d0c55a957ab38299e6b3cb35e93bb0636fdc2ec
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-23 13:10:27 +0100

    feat: add badwords.php

--- badwords.php
+++ badwords.php
@@ -1,13 +1,50 @@
+<?php
+
+$paragrafo=$_GET["paragrafo"];
+$badword=$_GET["badword"];
+$paroladacensurare= str_replace($badword, '***', $paragrafo);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form PHP</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark">
+    <div class="container">
+        <div class="border rounded-4 p-5 my-5">
+            <h1 class="text-center text-success">Paragrafo utente</h1>
+
+            <p class="text-success"> Il paragrafo iniziale è:
+                <?php echo $paragrafo ?>
+            </p>
+
+            <p class="text-success">La lunghezza del paragrafo è:
+                <?php echo strlen ($paragrafo) ?>
+            </p>
+        </div>
+
+        <div class="border-rounded-4 p-5 my-5">
+            <p class="text-success text-center mb-5 fw-bold">La parola da censurare è:
+                <?php echo $badword ?>
+            </p>
+
+            <p class="text-success">Il paragrafo censurato è:
+                <?php echo $paroladacensurare ?>
+            </p>
+
+            <p class="text-center text-success my-3">La lunghezza del paragrafo è:
+                <?php echo strlen ($paroladacensurare) ?>
+            </p>
+        </div>
+    </div>
 
 
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,22 +7,22 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark">
     <div class="container">
         <div class="row d-flex justify-content-center">
-            <h1 class="text-center">PHP_BADWORDS</h1>
+            <h1 class="text-center text-success">PHP_BADWORDS</h1>
             <div class="col-6">
-                <form action="badwords.php" method="get">
+                <form action="badwords.php" method="GET">
                     <div>
-                        <label class="text-success fw-bold fs-3 my-4" for="paragraph">Inserisci il paragrafo:</label>
-                        <textarea class="form-control mb-4 w-100 bg-success border-dark" name="paragraph" id="paragraph" placeholder="Scrivi un paragrafo"></textarea>
+                        <label class="text-success fw-bold fs-3 my-4" for="paragrafo">Inserisci il paragrafo:</label>
+                        <textarea class="form-control mb-4 w-100 bg-success border-white" name="paragrafo" id="paragrafo" placeholder="Scrivi un paragrafo"></textarea>
                     </div>
     
                     <div>
-                        <label class="text-white mb-2" for="paragraph">Inserisci la parola da censurare</label>
+                        <label class="text-success mb-2 fw-bold fs-3" for="paragrafo">Inserisci la parola da censurare</label>
                     </div>
     
-                    <input class="mb-4 form-control bg-success border-dark" id="paragraph" type="text" name="badword" placeholder="Inserisci la parola da censurare">
+                    <input class="mb-4 form-control bg-success border-white" id="paragrafo" type="text" name="badword" placeholder="Inserisci la parola da censurare">
 
                     <div class="text-center">
                         <button type="submit" class="btn btn-outline-success">Invia</button>
